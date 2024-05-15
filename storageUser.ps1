254324d8fbd9fb33e303b74dfe039c41e088a726
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-05-15 15:15:58 -0600

    Add files via upload

---
+++ storageUser.ps1
@@ -0,0 +1,9 @@
+﻿$computers = "BGC6746", "BGC4388"  # Add the names of the computers you want to check
+
+foreach ($computer in $computers) {
+    $diskInfo = Get-WmiObject Win32_LogicalDisk -ComputerName $computer -Filter "DriveType=3" | Select-Object DeviceID, @{Name="FreeSpace(GB)"; Expression={[math]::Round($_.FreeSpace / 1GB, 2)}}, @{Name="TotalSize(GB)"; Expression={[math]::Round($_.Size / 1GB, 2)}}
+
+    Write-Host "Storage information for"
+    $diskInfo | Format-Table -AutoSize
+    Write-Host ""
+}
