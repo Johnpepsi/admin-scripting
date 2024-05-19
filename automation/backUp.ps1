c20de281a5f3e83eae10c4f628b4f8091b2aaee0
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-05-19 10:47:44 -0600

    Create backUp.ps1

---
+++ automation/backUp.ps1
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Variables
+SOURCE_DIR="/home/user/data"
+DESTINATION_DIR="user@backupserver:/backup/data"
+LOG_FILE="/var/log/backup.log"
+
+# Run rsync to backup data
+rsync -avh --delete $SOURCE_DIR $DESTINATION_DIR >> $LOG_FILE 2>&1
+
+# Log completion time
+echo "Backup completed on $(date)" >> $LOG_FILE
