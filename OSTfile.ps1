d9f07a9db3352d6deae84f07886e0920332da8e6
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-05-21 08:48:40 -0600

    Create OSTfile.ps1

---
+++ OSTfile.ps1
@@ -0,0 +1,16 @@
+# Define the path to the OST file location
+# This is how to check own OST file
+$ostFilePath = "$env:LOCALAPPDATA\Microsoft\Outlook"
+
+# Get the list of OST files in the directory
+$ostFiles = Get-ChildItem -Path $ostFilePath -Filter *.ost
+
+# Check if any OST files are found and display their size
+if ($ostFiles) {
+    foreach ($file in $ostFiles) {
+        $fileSizeMB = [math]::Round($file.Length / 1MB, 2)
+        Write-Host "OST File: $($file.FullName) - Size: $fileSizeMB MB"
+    }
+} else {
+    Write-Host "No OST files found in the default location."
+}
