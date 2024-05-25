26932f22a6eb928856ab41cecba38e396010c835
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-05-25 11:04:34 -0600

    Update storageUser.ps1

--- storageUser.ps1
+++ storageUser.ps1
@@ -1,4 +1,4 @@
-﻿$computers = "BGC6746", "BGC4388"  # Add the names of the computers you want to check
+﻿$computers = "", ""  # Add the names of the computers you want to check
 
 foreach ($computer in $computers) {
     $diskInfo = Get-WmiObject Win32_LogicalDisk -ComputerName $computer -Filter "DriveType=3" | Select-Object DeviceID, @{Name="FreeSpace(GB)"; Expression={[math]::Round($_.FreeSpace / 1GB, 2)}}, @{Name="TotalSize(GB)"; Expression={[math]::Round($_.Size / 1GB, 2)}}
