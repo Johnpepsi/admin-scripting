eaa5bbf7f0671cd62fc5b9c76332f846ec541696
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-05-25 11:16:11 -0600

    Update forceUpdate.ps1

--- automation/forceUpdate.ps1
+++ automation/forceUpdate.ps1
@@ -1,8 +1,22 @@
-# Scenario: Automating the deployment of updates and patches to servers and workstations.
-# Example Script: A PowerShell script to update Windows systems.
-
-# Check for updates
-$updates = Get-WindowsUpdate -Install -AcceptAll -AutoReboot
-
-# Output the results
-$updates | Out-File -FilePath "C:\Logs\WindowsUpdate.log"
+@echo off
+Echo ------------------------------------------------------------------------------------------------------------
+Echo For a more thorough cleaning. Run this with Admin access
+Echo ------------------------------------------------------------------------------------------------------------
+pause
+del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
+"%ProgramFiles%\Windows Defender\MpCmdRun.exe" -removedefinitions -dynamicsignatures
+"%ProgramFiles%\Windows Defender\MpCmdRun.exe" -SignatureUpdate
+wuauclt.exe /updatenow
+cleanmgr /verylowdisk
+gpupdate /force
+winget upgrade --all --include-unknown
+Echo ------------------------------------------------------------------------------------------------------------
+Echo ------------------------------------------------------------------------------------------------------------
+Echo ------------------------------------------------------------------------------------------------------------
+Echo PC will Restart in 30 seconds if no key is pressed
+Echo ------------------------------------------------------------------------------------------------------------
+Echo ------------------------------------------------------------------------------------------------------------
+Echo ------------------------------------------------------------------------------------------------------------
+Shutdown -r -f -t 30
+pause
+Shutdown -a
