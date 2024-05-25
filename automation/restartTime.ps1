121a4a169f31ba1cb97081735e527b2fe3c70a50
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-05-25 14:40:31 -0600

    Create restartTime.ps1

---
+++ automation/restartTime.ps1
@@ -0,0 +1,24 @@
+# This script is to see when was the last time user restarted their machine
+function Get-Uptime {
+  [CmdletBinding()]
+  param ()
+    [string]$ComputerName = 'localhost'
+}
+
+foreach ($Computer in $ComputerName){
+  $pc = $computername
+  $os = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computername
+  $diff = $os.ConverToDateTime($os.LocalDateTime) -$os.ConvertToDateTime($os.LocalDateTime)
+
+  $properties=@{
+    'ComputerName'=$pc;
+    'UptimeDays'=$diff.Days;
+    'UptimeHours'=$diff.Hours;
+    'UptimeMinutes'=$diff.Minutes;
+    'UptimeSeconds'=$diff.Seconds;
+  }
+  $obj = New-Ojbect -TypeName PSObject -Property $properties
+
+  Write-Output $obj
+  }
+}
