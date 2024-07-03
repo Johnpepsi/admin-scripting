7c2f33243478683684dff586afd7c13157281390
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-07-03 09:52:51 -0600

    Update storageUser.ps1

--- storageUser.ps1
+++ storageUser.ps1
@@ -7,3 +7,24 @@ foreach ($computer in $computers) {
     $diskInfo | Format-Table -AutoSize
     Write-Host ""
 }
+
+-------------------------------------------------------------------------------------------------
+# another way to check when logged in to user machine to check storage 
+How to check user storage using shell
+# Get-StorageUsage.ps1
+
+# Function to get storage usage
+function Get-StorageUsage {
+    Get-PSDrive -PSProvider FileSystem | ForEach-Object {
+        [pscustomobject]@{
+            Drive = $_.Name
+            UsedSpaceGB = [math]::round(($_.Used / 1GB), 2)
+            FreeSpaceGB = [math]::round(($_.Free / 1GB), 2)
+            TotalSpaceGB = [math]::round(($_.Used + $_.Free) / 1GB, 2)
+            PercentUsed = [math]::round(($_.Used / ($_.Used + $_.Free) * 100), 2)
+        }
+    }
+}
+
+# Display storage usage
+Get-StorageUsage | Format-Table -AutoSize
