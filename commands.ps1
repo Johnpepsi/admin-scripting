e707e3c903574e517fac1772a978ed3c52614d33
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-08-08 11:11:36 -0600

    Update and rename pcname.ps1 to commands.ps1

--- commands.ps1
+++ commands.ps1
@@ -5,3 +5,8 @@ pc name -> hostname
 
 qwinsta /server:<serverName> 
 rwinsta sessionID (rwinsta 13)
+
+# Shortcut keys windows
+
+cntrl + shift = moving to the next tab.
+cntrl + shift + tab = move to the previous tab.
