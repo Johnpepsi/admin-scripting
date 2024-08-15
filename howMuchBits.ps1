fc5f286e32374ad8abbd797f52944bee022c36d9
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-08-15 09:22:15 -0600

    Create howMuchBits.ps1

---
+++ howMuchBits.ps1
@@ -0,0 +1,5 @@
+# Check the OS architecture (32-bit or 64-bit)
+$architecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+
+# Output the result
+Write-Output "Your machine is running a $architecture operating system."
