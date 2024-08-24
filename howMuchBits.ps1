f70271d186f7979ef122f0a63b18468ba9995f65
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-08-24 08:07:33 -0600

    Update howMuchBits.ps1

--- howMuchBits.ps1
+++ howMuchBits.ps1
@@ -1,4 +1,4 @@
-# Check the OS architecture (32-bit or 64-bit)
+# How to check the Operating System architecture (32-bit or 64-bit)
 $architecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
 
 # Output the result
