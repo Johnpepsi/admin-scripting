ef1359b766235a7ff9c7679683b8ec8062e099c8
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-10-07 11:37:57 -0600

    Create ScriptingAD.ps1

---
+++ Homelab/ScriptingAD.ps1
@@ -0,0 +1,28 @@
+# Import required modules 
+Import-module ActiveDirectory   
+# Create a new password 
+$securePassword = ConvertTo-SecureString "Brandt20288!!" -AsPlainText -Force 
+
+# Prompt user for CSV File pathaddAccount 
+$filepath = Read-Host -Prompt "Please enter the path of your CV File" 
+
+# Import the file into a variable 
+$users = Import-Csv $filepath 
+
+# Loop through each row and gather information 
+ForEach ($user in $users) { 
+
+    # Gather the user's information 
+    $fname = $user.'First Name' 
+    $lname = $user.'Last Name' 
+    $jtitle = $user.'Job Title' 
+    $officephones = $user.'Office Phones' 
+    $emailaddress = $user.'Description' 
+    $OrganizationalUnit = $user.'Organizational Unit' 
+
+    # Create the new AD user for each user in CSV File 
+    New-ADUser -Name "$fname $lname" -GivenName $fname -Surname $lname -UserPrincipalName "$fname.$lname" -Path $OrganizationalUnit -AccountPassword $securePassword -ChangePasswordAtLogon $True -OfficePhone $officephones -Description $description -Enabled $True -EmailAddress $emailaddress 
+    # Echo output for each new user 
+    echo "Account created for $fname $lname in $OrganizationalUnit" 
+
+} 
